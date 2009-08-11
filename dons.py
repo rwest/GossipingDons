14e07f745222968f5fc293768d584f4cbb6753a4
Richard West <rwest@mit.edu>
2009-08-11 10:17:43 -0400

    Fix a bug (g called f when trying to recurse), renamed the functions, and made it more general (can do N not a power of 2)

--- dons.py
+++ dons.py
@@ -1,16 +1,21 @@
 
 
-def f(n):
+def circle(n):
 	"""The simple call-the-guy-on-your-right-twice algorithm"""
 	return 2*n-1
 
-def g(n):
+def divide(n):
 	"""My clever algorithm where you divide into two groups who then call each other"""
+	if n==1: return 0
 	if n==2: return 1
-	return 2*f(n/2) + n/2
+	if n==3: return 3
+	smallgroup = int(n/2.0)
+	biggroup = int(n/2.0 + 0.5)
+	return divide(smallgroup) + divide(biggroup) + smallgroup
 
 
-for i in range(1,10):
-	a=2**i
-	print 'n=%d, f(n)=%d, g(n)=%d'%(a, f(a), g(a))
+print "n	circle	divide"
+for i in range(2,100):
+	n=i
+	print '%d\t%d\t%d'%(n, circle(n), divide(n))
 
